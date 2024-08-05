6ac9d3051924369c42a8e34b34f5b2bab6ee9fb2
AmazingRobin <robinli0302@gmail.com>
2024-08-05 19:59:13 +0800

    初始化

--- src/renderer/src/main.js
+++ src/renderer/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import '@renderer/assets/cust-elementplus.scss'
+import '@renderer/assets/icon/iconfont.css'
+import '@renderer/assets/base.scss'
 import router from './router'
 
 const app = createApp(App)
